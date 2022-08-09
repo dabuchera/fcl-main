3bbcf415667e47ceae4a85001fde38412f6e8c2a
David <bucher@ibi.baug.ethz.ch>
2022-08-09 11:50:32 +0200

    init

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,21 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import Layout from '../components/Layout'
+import TransactionToastProvider from '../providers/TransactionToastProvider'
+import { Toaster } from 'react-hot-toast'
+import StacksProvider from '../providers/StacksProvider'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <StacksProvider>
+      <TransactionToastProvider>
+        <Toaster position="bottom-right" />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </TransactionToastProvider>
+    </StacksProvider>
+  )
+}
+
+export default MyApp
